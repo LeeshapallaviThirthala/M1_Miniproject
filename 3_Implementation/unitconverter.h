8b627bc0af2b9d83e65c107af644b80d6af84dea
LeeshapallaviThirthala <101167273+LeeshapallaviThirthala@users.noreply.github.com>
2022-04-04 15:08:59 +0530

    Add files via upload

---
+++ 3_Implementation/unitconverter.h
@@ -0,0 +1,14 @@
+#ifdef __UnitConvertor
+#define __UnitConvertor
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include <windows.h>
+
+
+void menu();
+void tempconvertor(int n);
+void currencyconvertor(int n);
+void massconvertor(int n);
+
+#endif
